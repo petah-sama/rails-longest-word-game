91c72b5efee11e96a032d9254fd61e7b87154cd5
Pedro Batista <pedrobatista.420@gmail.com>
2020-02-17 17:47:29 +0000

    some styling applied

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,5 +1,4 @@
 require 'open-uri'
-require 'json'
 
 class GamesController < ApplicationController
   def new
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root to: 'games#new'
+
   get 'new', to: 'games#new'
   get 'score', to: 'games#score'
   post 'score', to: 'games#score'
